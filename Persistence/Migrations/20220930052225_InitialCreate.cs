c02f5a2982477ed11ea878493c2c7a1600311f36
murosa <amuros9100@gmail.com>
2022-09-30 19:36:33 -0500

    Database for project  created

---
+++ Persistence/Migrations/20220930052225_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TriviaGames",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Question = table.Column<string>(type: "TEXT", nullable: true),
+                    Answer1 = table.Column<string>(type: "TEXT", nullable: true),
+                    Answer2 = table.Column<string>(type: "TEXT", nullable: true),
+                    Answer3 = table.Column<string>(type: "TEXT", nullable: true),
+                    Answer4 = table.Column<string>(type: "TEXT", nullable: true),
+                    CorrectAnswer = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TriviaGames", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TriviaGames");
+        }
+    }
+}
